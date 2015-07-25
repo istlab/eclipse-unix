829f301904db2ee5f82bde772d3e782ec858aa1c
htsili <htsiligiannis@gmail.com>
2015-07-25 08:49:21 +0300

    Minor format changes

--- eclipse-unix-plugin/src/main/java/gr/aueb/dmst/istlab/unixtools/io/impl/FileStreamProvider.java
+++ eclipse-unix-plugin/src/main/java/gr/aueb/dmst/istlab/unixtools/io/impl/FileStreamProvider.java
@@ -1,20 +1,21 @@
 package gr.aueb.dmst.istlab.unixtools.io.impl;
 
-import gr.aueb.dmst.istlab.unixtools.io.IOStreamProvider;
-
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import gr.aueb.dmst.istlab.unixtools.io.IOStreamProvider;
+
 public final class FileStreamProvider implements IOStreamProvider {
 
   private String filename;
 
   public FileStreamProvider(String filename) {
-    if (filename == null)
+    if (filename == null) {
       throw new IllegalArgumentException();
+    }
 
     this.filename = filename;
   }
