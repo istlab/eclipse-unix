c4860cf9b0bd548b182b727a10bd547473fd59bd
vvalvis <v.valvis@hotmail.gr>
2015-07-27 15:22:49 +0300

    Added CustomCommandHandler class.
    This class will handle any eclipse commands we create for the package
    explorer ui. It is responsible for the selected command's execution.

---
+++ eclipse-unix-plugin/src/main/java/gr/aueb/dmst/istlab/unixtools/handlers/CustomCommandHandler.java
@@ -0,0 +1,85 @@
+package gr.aueb.dmst.istlab.unixtools.handlers;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.commons.lang3.SystemUtils;
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+import gr.aueb.dmst.istlab.unixtools.actions.ActionExecutionCallback;
+import gr.aueb.dmst.istlab.unixtools.actions.DataActionResult;
+import gr.aueb.dmst.istlab.unixtools.actions.impl.ExecuteCustomCommandAction;
+import gr.aueb.dmst.istlab.unixtools.core.model.CustomCommand;
+import gr.aueb.dmst.istlab.unixtools.factories.ActionFactorySingleton;
+
+public class CustomCommandHandler extends AbstractHandler {
+
+  private CustomCommand cc;
+
+  public CustomCommandHandler(CustomCommand cc) {
+    this.cc = cc;
+  }
+
+  @Override
+  public Object execute(ExecutionEvent event) throws ExecutionException {
+    ExecuteCustomCommandAction action = (ExecuteCustomCommandAction) ActionFactorySingleton.INSTANCE
+        .createCustomCommandExecuteAction(cc);
+    try {
+      action.execute(new ActionExecutionCallback<DataActionResult<InputStream>>() {
+
+        @Override
+        public void onCommandExecuted(DataActionResult<InputStream> result) {
+          BufferedReader br = null;
+          BufferedWriter bw = null;
+          String line = null;
+          try {
+            br = new BufferedReader(new InputStreamReader(result.getData()));
+            if (cc.isOutputToConsole()) {
+              while ((line = br.readLine()) != null) {
+                System.out.println(line);
+              }
+            } else {
+              File outputFile = new File(cc.getOutputFilename());
+              // if file doesn't exists, then create it
+              if (!outputFile.exists()) {
+                outputFile.createNewFile();
+              }
+              bw = new BufferedWriter(new FileWriter(outputFile));
+
+              while ((line = br.readLine()) != null) {
+                bw.write(line);
+                if (SystemUtils.IS_OS_WINDOWS)
+                  bw.write("\r\n");
+                if (SystemUtils.IS_OS_LINUX)
+                  bw.write("\n");
+              }
+            }
+          } catch (IOException io) {
+            io.printStackTrace();
+          } finally {
+            try {
+              if (br != null)
+                br.close();
+              if (bw != null)
+                bw.close();
+            } catch (IOException e) {
+              e.printStackTrace();
+            }
+          }
+        }
+      });
+    } catch (IOException e) {
+      e.printStackTrace();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    return null;
+  }
+}
